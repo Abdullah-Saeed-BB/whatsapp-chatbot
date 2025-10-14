df0c4b5b89c796518b78314bf33f62585d0d22c7
Abdullah Saeed <bnbshrbconect@gmail.com>
2025-10-14 10:11:08 +0300

    Disable the spam

--- routes/whatsapp.py
+++ routes/whatsapp.py
@@ -32,12 +32,21 @@ def whatsapp_webhook():
         print(e)
         res = "Sorry, there error occures while generate the response. Please try again later.\n\nالمعذرة, ولكن هنالك خطأ حدث اثناء إنشاء الرد. الرجاء المحاولة مرة اخرى لاحقاً"
     
-    resp.message(res)
+    if type(res) == str:
+        resp.message(res)
+    else:
+        for message in res:
+            resp.message(message)
 
     return str(resp)
 
+maximum_messages = 18
+
 def generate_response(body, sender, user_name):
     contents = get_history(sender, body, user_name)
+
+    if len(contents) > maximum_messages:
+        return "Sorry, but you have reached the maximum messages. Try again later.\n\nالمعذرة, لقد وصلت الحد الأقصلا من الرسائل. حاول مرة اخرى لاحقاً."
 
     if not model or not current_app.config["is_sys_instruction_updated"]:
         load_model()
@@ -49,6 +58,13 @@ def generate_response(body, sender, user_name):
     })
 
     if model_res.candidates and model_res.candidates[0].content.parts:
+        if len(contents) > maximum_messages - 2:
+            return [
+                model_res.text,
+                f"You only got {maximum_messages - len(contents)} messages left\
+                \nلديك فقد {maximum_messages - len(contents)} رسائل متبقية"
+            ]
+
         save_history(sender, contents, model_res.text)
         return model_res.text
     raise Exception("Model didn't response")
